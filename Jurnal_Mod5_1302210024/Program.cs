41a4df33689aeee56d01f4bd05abacbc3e56d6f9
HelloWorldYessir420 <119160448+HelloWorldYessir420@users.noreply.github.com>
2023-03-21 17:04:38 +0700

    main program

---
+++ Jurnal_Mod5_1302210024/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Jurnal_Mod5_1302210024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Penjumlahan tambah = new Penjumlahan();
+            tambah.JumlahTigaAngka<double>(13, 02, 21);
+
+            Console.WriteLine("");
+
+            SimpleDataBase<double> data = new SimpleDataBase<double>();
+            data.addNewData(13);
+            data.addNewData(02);
+            data.addNewData(21);
+            data.printAllData();
+        }
+
+    }
+}
